6062055a98779db7566c37e1fc2adefa4b2034da
sudoapt <pramuk@live.ru>
2023-12-13 12:13:33 +1000

    build.gradle.kts. configured

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,19 @@
 plugins {
-    id("java")
-}
+    application
+//    id("java")
 
+
+}
 group = "hexlet.code"
+
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.test {
