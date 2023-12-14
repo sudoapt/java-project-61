3fa547f7d3b637221c50101499cbc9c5e316485b
sudoapt <pramuk@live.ru>
2023-12-14 16:51:06 +1000

    foo

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Cli {
     public void greetUser() {
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("May i have your name?");
 
         Scanner sc = new Scanner(System.in);
