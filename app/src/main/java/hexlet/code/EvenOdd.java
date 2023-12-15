489fcaca4abca6077327400a31ac88932b9ef003
sudoapt <pramuk@live.ru>
2023-12-15 11:37:54 +1000

    eevenodd class is funcitonal

--- app/src/main/java/hexlet/code/EvenOdd.java
+++ app/src/main/java/hexlet/code/EvenOdd.java
@@ -5,20 +5,30 @@ import java.util.Scanner;
 public class EvenOdd {
     public void checkEvenOdd(String name) {
         boolean isEven;
-        Random random = new Random();
-        int rnd = random.nextInt(100) + 1;
-        isEven = rnd % 2 == 0;
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.\nQuestion: " + rnd);
-        String answer = sc.nextLine();
+        int answersGiven = 0;
+
+
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        while (answersGiven < 3) {
+            Random random = new Random();
+            int rnd = random.nextInt(100) + 1;
+            isEven = rnd % 2 == 0;
+
+            System.out.println("Question: " + rnd);
+            String answer = sc.nextLine();
+            System.out.println("Your answer is: " + answer);
+            if ((isEven && answer.equals("yes") || (!isEven && answer.equals("no")))) {
+                System.out.println("Correct");
+            } else {
+                System.out.println(answer + " is a wrong answer ;(. The correct answer was 'no'.");
+                System.out.println("Let's try again, " + name);
+            }
+            ++answersGiven;
+        }
+        System.out.println("Congats, " + name + "!");
 
 //        check
-        if ((isEven && answer.equals("yes") || (!isEven && answer.equals("no")))) {
-            System.out.println("Correct");
-        } else {
-            System.out.println(answer + " is a wrong answer ;(. The correct answer was 'no'.");
-            System.out.println("Let's try again, " + name);
-        }
+
     }
 }
