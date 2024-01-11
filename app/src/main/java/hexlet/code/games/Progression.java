f72708a23ba4dadd6a7a2d2ec34ca36a1504097f
sudoapt <pramuk@live.ru>
2024-01-11 16:42:53 +1000

    Progression refactored

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,31 +1,44 @@
 package hexlet.code.games;
+import hexlet.code.Engine;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Progression {
     public static String gameRule() {
         return "What number is missing in the progression?";
     }
-    public static ArrayList<Integer> buildAProgression(int bound, int length) {
-        ArrayList<Integer> progression = new ArrayList<>();
+    public static ArrayList<String> getProgression(int bound, int length) {
+        ArrayList<String> progression = new ArrayList<>();
         for (int i = 0; i < length; i++) {
-            progression.add(bound + (i * 2));
+            progression.add(Integer.toString((bound + (i * 2))));
             /* 1) bound + (1*2) = 7
             2) bound + (2*2) = 9 */
         }
         return progression;
     }
 
-    public static int pickIndexToReplace(ArrayList<Integer> progression) {
-        Random random = new Random();
-        return random.nextInt(progression.size()); // an index if an int ot hide
+    public static void runProgressionGame() {
+        String[][] gameData = new String[Engine.ROUNDS_AMOUNT][Engine.AMOUNT_OF_GAME_UNITS];
+        for (String[] gameDatum: gameData) {
+            int progressionStartNum = Engine.randInt(100);
+            int progressionLength = Engine.randInt(6) + 5;
+            ArrayList<String> progression = getProgression(progressionStartNum, progressionLength);
+            int indexToHide = Engine.randInt(progression.size());
+            String question = progressionWithHidedNum(progression, indexToHide);
+            gameDatum[Engine.QUESTION_INDEX] = question;
+            gameDatum[Engine.ANSWER_INDEX] = getAnswer(progression, indexToHide);
+        }
+        Engine.runGame(gameRule(), gameData);
+
+
     }
 
-    public static int getHidedNumber(ArrayList<Integer> progression, int index) {
+
+    public static String getAnswer(ArrayList<String> progression, int index) {
         return progression.get(index); // returns the actual int
     }
 
-    public static String toString(ArrayList<Integer> progression, int indexToHide) {
+    public static String progressionWithHidedNum(ArrayList<String> progression, int indexToHide) {
+//        int indexToHide = Engine.randInt(progression.size());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < progression.size(); i++) {
             if (i == indexToHide) {
@@ -35,9 +48,5 @@ public class Progression {
             }
         }
         return sb.toString().trim();
-    }
-
-    public static boolean checkAnswer(int userAnswer, int hidedNumber) {
-        return userAnswer == hidedNumber;
     }
 }
