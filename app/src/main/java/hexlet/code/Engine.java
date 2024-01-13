026e6b6f7eae8916f6e80b438206e21d272252e4
sudoapt <pramuk@live.ru>
2024-01-13 14:37:03 +1000

    user input fixed

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -9,7 +9,7 @@ public class Engine {
 
     public static void runGame(String rules, String[][] gameData) {
         System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
+        System.out.print("May I have your name? ");
         Scanner sc = new Scanner(System.in);
         String userName = sc.nextLine();
         System.out.println("Hello, " + userName + "!");
