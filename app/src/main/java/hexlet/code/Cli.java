e691bb8427401cfb39d0ac8cf21ff860ac28b87a
sudoapt <pramuk@live.ru>
2024-01-14 14:02:39 +1000

    cli i capitalized

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Cli {
     public static void greetUser() {
         System.out.println("Welcome to the Brain Games!");
-        System.out.println("May i have your name?");
+        System.out.println("May I have your name?");
         Scanner sc = new Scanner(System.in);
         String userName = sc.nextLine();
         System.out.println("Hello, " + userName + "!");
